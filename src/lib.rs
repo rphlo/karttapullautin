d21d967b7970d50f322a15a859914153f3317308
antbern <40672068+antbern@users.noreply.github.com>
2024-10-11 10:45:20 +0200

    create Vec2D type as two-dim vector using contigous memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 // we use a lot of manual indices instead of `take` and `skip`, so allow that
 #![allow(clippy::needless_range_loop)]
+// make sure any use of unsafe is documented
+#![deny(clippy::undocumented_unsafe_blocks)]
 
 pub mod blocks;
 pub mod canvas;
@@ -12,4 +14,5 @@ pub mod merge;
 pub mod process;
 pub mod render;
 pub mod util;
+pub mod vec2d;
 pub mod vegetation;
