e45783d50801c5edd81eba20214acbc8d3b705b6
antbern <40672068+antbern@users.noreply.github.com>
2024-11-14 16:07:04 +0100

    add basic HeightMap container and trait for converting to/from bytes

---
+++ src/io/bytes.rs
@@ -0,0 +1,30 @@
+/// Trait defining how to read and write a value from a byte stream.
+pub trait FromToBytes: Sized {
+    /// Read a value from a byte stream.
+    fn from_bytes<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self>;
+
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()>;
+}
+
+impl FromToBytes for f64 {
+    fn from_bytes<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let mut buff = [0; 8];
+        reader.read_exact(&mut buff)?;
+        Ok(f64::from_ne_bytes(buff))
+    }
+
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(&self.to_ne_bytes())
+    }
+}
+
+impl FromToBytes for usize {
+    fn from_bytes<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let mut buff = [0; 8];
+        reader.read_exact(&mut buff)?;
+        Ok(usize::from_ne_bytes(buff))
+    }
+    fn to_bytes<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(&self.to_ne_bytes())
+    }
+}
