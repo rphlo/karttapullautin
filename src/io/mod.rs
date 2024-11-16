b8a5ef5d91adfd4bd9b52249f96a7e5854796fb1
antbern <40672068+antbern@users.noreply.github.com>
2024-11-16 23:20:40 +0100

    add support for converting internal .hmap files to .xyz text-based files

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -4,26 +4,39 @@ use std::{
     path::Path,
 };
 
+use heightmap::HeightMap;
+
 pub mod bytes;
 pub mod heightmap;
 pub mod xyz;
 
 /// Helper function to convert an internal xyz file to a regular xyz file.
 pub fn internal2xyz(input: &str, output: &str) -> std::io::Result<()> {
-    let mut reader = xyz::XyzInternalReader::open(Path::new(input))?;
-    let mut writer = BufWriter::new(File::create(output)?);
+    if input.ends_with(".xyz.bin") {
+        let mut reader = xyz::XyzInternalReader::open(Path::new(input))?;
+        let mut writer = BufWriter::new(File::create(output)?);
 
-    while let Some(record) = reader.next()? {
-        writeln!(
-            writer,
-            "{} {} {} {} {} {}",
-            record.x,
-            record.y,
-            record.z,
-            record.classification,
-            record.number_of_returns,
-            record.return_number
-        )?;
+        while let Some(record) = reader.next()? {
+            writeln!(
+                writer,
+                "{} {} {} {} {} {}",
+                record.x,
+                record.y,
+                record.z,
+                record.classification,
+                record.number_of_returns,
+                record.return_number
+            )?;
+        }
+    } else if input.ends_with(".hmap") {
+        let hmap = HeightMap::from_file(input)?;
+        let mut writer = BufWriter::new(File::create(output)?);
+
+        for (x, y, h) in hmap.iter() {
+            writeln!(writer, "{} {} {}", x, y, h)?;
+        }
+    } else {
+        panic!("Unknown internal file format: {}", input);
     }
 
     Ok(())
