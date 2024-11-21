fb49d415cddb4df42d8cfad4692cbef8a2a48e96
Carlo Giacometti <carlogico@gmail.com>
2024-11-21 13:35:17 -0700

    add a -v parameter to regression.sh to control the version to compare
    example usage: `➜ tests/regression.sh -v v2.4.3`
    compares the current build to v.2.4.3

--- tests/regression.sh
+++ tests/regression.sh
@@ -1,5 +1,42 @@
 #! /bin/bash
 
+VERSION="latest"
+while getopts ":v:" opt; do
+  case $opt in
+    v) VERSION="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+if [[ "$VERSION" != "latest" ]]; then
+  read -ra RELEASES <<< $(curl https://api.github.com/repos/rphlo/karttapullautin/releases | jq '.[] | .tag_name'  | tr -d '"')
+  IFS=' '
+  # Flag for finding the element
+  found=false
+  # Loop through the array
+  for element in "${RELEASES[@]}"; do
+    if [[ "$element" == "$VERSION" ]]; then
+      found=true
+      break
+    fi
+  done
+
+  # Output the result
+  if [[ "$found" == false ]]; then
+    echo "$VERSION is not a valid version."
+    exit 1
+  fi
+fi
+
 # Get the directory of the currently executing script
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
@@ -34,7 +71,7 @@ fi
 
 cd data
 
-echo "\n############ Running the current build of pullauta ############]/n"
+echo -e "\n############ Running the current build of pullauta ############\n"
 
 WORK_DIR="CurrentBranch"
 if [ ! -d "$WORK_DIR" ]; then
@@ -62,7 +99,7 @@ cd "$WORK_DIR"
 
 cd ..
 
-echo "\n############ Running the latest release of pullauta ############]/n"
+echo -e "\n############ Running the $VERSION release of pullauta ############\n"
 
 RELEASE_DIR="Release"
 if [ ! -d "$RELEASE_DIR" ]; then
@@ -79,13 +116,17 @@ fi
 
 cd "$RELEASE_DIR"
 
-LATEST_TAG=$(curl -sL https://api.github.com/repos/rphlo/karttapullautin/releases/latest | jq -r ".tag_name")
-if [ ! -d "$LATEST_TAG" ]; then
+if [[ "$VERSION" == "latest" ]]; then
+  TAG==$(curl -sL https://api.github.com/repos/rphlo/karttapullautin/releases/latest | jq -r ".tag_name")
+else
+  TAG="$VERSION"
+fi
+if [ ! -d "$TAG" ]; then
     echo "Directory does not exist. Creating..."
-    mkdir -p "$LATEST_TAG"
+    mkdir -p "$TAG"
     if [ $? -eq 0 ]; then
-        echo "Directory created: $LATEST_TAG"
-        cd "$LATEST_TAG"
+        echo "Directory created: $TAG"
+        cd "$TAG"
         # Detect the operating system
         OS=""
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -111,20 +152,27 @@ if [ ! -d "$LATEST_TAG" ]; then
             exit 1
         fi
         FILE_NAME="karttapullautin-${ARCH}-${OS}.tar.gz"
-        curl -L "https://github.com/rphlo/karttapullautin/releases/latest/download/$FILE_NAME" | tar xvz
+
+        if [[ "$VERSION" == "latest" ]]; then
+          URL="https://github.com/rphlo/karttapullautin/releases/latest/download/$FILE_NAME"
+        else
+          URL="https://github.com/rphlo/karttapullautin/releases/download/$TAG/$FILE_NAME"
+        fi
+
+        curl -L $URL | tar xvz
+
         ./pullauta ../../test_file.laz
         cd ..
     else
         echo "Failed to create directory."
     fi
 else
-    echo "Directory already exists: $LATEST_TAG"
+    echo "Directory already exists: $TAG"
 fi
-
 
 cd ..
 
-echo "\n############ Comparing the outputs ############]n"
+echo -e "\n############ Comparing the outputs ############\n"
 
-pngcomp "$WORK_DIR/pullautus.png" "$RELEASE_DIR/$LATEST_TAG/pullautus.png" | tee -a pngcomp.txt
-pngcomp "$WORK_DIR/pullautus_depr.png" "$RELEASE_DIR/$LATEST_TAG/pullautus_depr.png" | tee -a pngcomp_depr.txt
+pngcomp "$WORK_DIR/pullautus.png" "$RELEASE_DIR/$TAG/pullautus.png" | tee -a pngcomp.txt
+pngcomp "$WORK_DIR/pullautus_depr.png" "$RELEASE_DIR/$TAG/pullautus_depr.png" | tee -a pngcomp_depr.txt
